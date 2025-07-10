b697e03409179aa6ea5d44ba99befb900ea0008c
danilo <danilocencigg@gmail.com>
2025-07-10 20:27:03 -0300

    fixed/re-implemented projects section (html, js, assets)

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const dadosCardControleMedicamentos = {
-    titulo: 'Controle de Medicamentos',
-    imagem: './assets/controle-de-medicamentos.png',
-    gif: 'https://i.imgur.com/SDgwx8m.gif',
-    urlRepositorio: 'https://github.com/academiadoprogramador-fullstack/controle-de-medicamentos-2025',
+    titulo: 'Gestão de Medicamentos',
+    imagem: './assets/gst_med.png',
+    urlRepositorio: 'https://github.com/Davetheguy1/Gest-oDeMedicamentosAp-WEB-',
     stack: [
         'https://skillicons.dev/icons?i=html&theme=light',
         'https://skillicons.dev/icons?i=css&theme=light',
         'https://skillicons.dev/icons?i=cs&theme=light',
         'https://skillicons.dev/icons?i=net&theme=light',
     ],
-    descricao: `O sistema de Controle de Medicamentos apresenta o conceito de gerenciamento de um posto de saúde, com o controle de fornecimento, estoque, funcionários, pacientes e retirada de prescrições — com foco na rastreabilidade das movimentações de estoque.`
 };
 
+const dadosCardCtrlBar = {
+    titulo: 'Controle de Bar',
+    imagem: './assets/ctrl_bar.png',
+    urlRepositorio: 'https://github.com/Davetheguy1/ControleDeBarAP',
+    stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+        'https://skillicons.dev/icons?i=bootstrap&theme=light'
+    ], 
+}
 
+const dadosCardEagenda = {
+    titulo: 'E-Agenda',
+    imagem: './assets/eAgenda.png',
+    urlRepositorio: 'https://github.com/Tiagao-com-T-ta/E-Agenda-Do-Tiagao',
+    stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=js&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+        'https://skillicons.dev/icons?i=bootstrap&theme=light'
+    ], 
+}
 
 let cardSelecionado;
 
@@ -23,12 +46,10 @@ function carregarDadosCardAtual() {
     const imgCard = document.getElementById('imgCard');
     const stackCard = document.getElementById('stackCard');
     const tituloCard = document.getElementById('tituloCard');
-    const descricaoCard = document.getElementById('descricaoCard');
     const urlRepositorio = document.getElementById('urlRepositorio');
 
     imgCard.src = cardSelecionado.imagem;
     tituloCard.textContent = cardSelecionado.titulo;
-    descricaoCard.textContent = cardSelecionado.descricao;
     urlRepositorio.href = cardSelecionado.urlRepositorio;
 
     stackCard.innerHTML = '';
@@ -51,26 +72,29 @@ function selecionarPrimeiroCard() {
 }
 
 function selecionarSegundoCard() {
-    if (cardSelecionado == dadosCardGestaoEquipamentos)
+    if (cardSelecionado == dadosCardCtrlBar)
         return;
 
-    cardSelecionado = dadosCardGestaoEquipamentos;
+    cardSelecionado = dadosCardCtrlBar;
     carregarDadosCardAtual();
 }
 
-function abrirModalProjeto() {
-    document.getElementById('portfolioModalLabel').textContent = cardSelecionado.titulo;
-    document.getElementById('portfolioModalImage').src = cardSelecionado.gif;
+function selecionarTerceiroCard() {
+    if (cardSelecionado == dadosCardEagenda)
+        return;
+
+    cardSelecionado = dadosCardEagenda;
+    carregarDadosCardAtual();
 }
 
 function main() {
     const btnSelecionarPrimeiroCard = document.getElementById("btnSelecionarPrimeiroCard");
     const btnSelecionarSegundoCard = document.getElementById("btnSelecionarSegundoCard");
-    const btnVerProjeto = document.getElementById("btnVerProjeto")
+    const btnSelecionarTerceiroCard = document.getElementById("btnSelecionarTerceiroCard");
 
     btnSelecionarPrimeiroCard.addEventListener('click', selecionarPrimeiroCard);
     btnSelecionarSegundoCard.addEventListener('click', selecionarSegundoCard);
-    btnVerProjeto.addEventListener('click', abrirModalProjeto);
+    btnSelecionarTerceiroCard.addEventListener('click', selecionarTerceiroCard);
 
     selecionarPrimeiroCard();
 };
